032ed5cc8f7f9abdda01d05436be3daa3739cb5e
Josh Date <jjdate@live.com>
2019-02-28 12:48:35 -0500

    done

---
+++ exercise.py
@@ -0,0 +1,11 @@
+
+
+def word_counter(letters):
+	print(len(letters.split()))
+
+
+word_counter("Hello world") # returns 2
+
+word_counter("This is a sentence") # returns 4
+
+word_counter("") # returns 0
